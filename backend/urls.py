50d828466abb79812e117bebcb800566a5151097
Nandini <nandinibansal1608@gmail.com>
2026-08-31 19:09:25 +0530

    functioning backend API

--- backend/urls.py
+++ backend/urls.py
@@ -29,9 +29,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # Route all requests starting with api/users/ to the users app!
     path('api/users/', include('users.urls')), 
+    path('api/', include('jobs.urls')), 
 ]
 
 if settings.DEBUG:
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Company, Job, Application
+from .serializers import CompanySerializer, JobSerializer, ApplicationSerializer
 
-# Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.filter(is_active=True).order_by('-created_at')
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(company=self.request.user.company)
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'employer':
+            return Application.objects.filter(job__company=user.company)
+        return Application.objects.filter(candidate=user)
+
+    def perform_create(self, serializer):
+        serializer.save(candidate=self.request.user)
